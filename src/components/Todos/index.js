15bcfd207d66275bf28f6b2127030655ec39cb9a
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-04-22 18:44:04 -0400

    index file updated

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,12 +1,47 @@
-import React from "react";
-import todoForm from "./todoForm";
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
-function Todos() {
+const initialState = [
+  { id: 1, message: "Walk the dog" },
+  { id: 2, message: "Take out trash" },
+  { id: 3, message: "Finish tutorial video" },
+];
+
+export default function Todos() {
+  const [todoList, setTodoList] = useState(initialState);
+
+  const deleteHandler = (id) => {
+    const newTodos = todoList.filter((item) => {
+      return item.id !== id;
+    });
+
+    setTodoList(newTodos);
+  };
+
+  const updateHandler = (todo) => {
+    setTodoList(
+      todoList.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            message: todo.message,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
   return (
     <div>
-      <todoForm />
+      <TodoForm todos={todoList} setTodos={setTodoList} />
+      <TodoList
+        todos={todoList}
+        deleteHandler={deleteHandler}
+        updateHandler={updateHandler}
+      />
     </div>
   );
 }
-
-export default Todos;
